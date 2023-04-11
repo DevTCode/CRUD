b02014281d4889d98c1f10bb664786381800a4fc
DevTCode <assimichraq@gmail.com>
2023-04-11 18:12:43 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,83 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
+  
+    
+    
+   
     public function index()
     {
-        return User::select('id','nom','prenom','CNE','tel','numPermis','email','password')->get();
+        return DB::table('users')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    
+    public function update(Request $request, User $User)
     {
-        //
-    }
+        $request->validate([
+           
+            'tel'=>'required',
+          
+            'email'=>'required',
+           
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        ]);
+        $User->fill($request->post())->update();
+        return response()->json([
+            'message'=>'User updated successfuly'
+        ]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function show(){
+        
+        return  DB::table('users')
+        ->select('tel','email')->get();
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+     
+      
+    public function destroy(Marque $Marque)
     {
-        //
+       
     }
   
 }
