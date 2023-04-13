8470b2945b6a0f37cc5f5d20be753903764d6285
DevTCode <assimichraq@gmail.com>
2023-04-13 03:45:50 +0200

    Update LocationController.php

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -11,49 +11,95 @@ class LocationController extends Controller
    
     public function index()
     {
-        $ids = [1, 2, 3, 4];
+        $ids = [1, 2, 3, 4,5];
     return  DB::table('locations')
-        ->join('cars', 'locations.car_id', '=', 'cars.id')
+        ->join('cars', 'locations.car_id', '=', 'cars.id')->join('images', 'cars.image_id', '=', 'images.id')
         ->join('users', 'locations.user_id', '=', 'users.id')
-        ->select('locations.idLocation','users.nom as nom','users.prenom as prenom','locations.dateL as dateL','locations.dateR as dateR','cars.prix as prix' )->whereIn('locations.idLocation', $ids)->orderBy('idLocation')->get();
+        
+        ->select('locations.id','users.nom as nom','users.prenom as prenom','images.chemin as image','locations.dateL as dateL','locations.dateR as dateR','cars.prix as prix' )->whereIn('locations.id', $ids)->orderBy('id')->get();
     }
     public function loc()
     {
         $ids = [4, 5, 6, 7];
     return  DB::table('locations')
-        ->join('cars', 'locations.car_id', '=', 'cars.id')
+        ->join('cars', 'locations.car_id', '=', 'cars.id')->join('images', 'cars.image_id', '=', 'images.id')
         ->join('users', 'locations.user_id', '=', 'users.id')
-        ->select('locations.idLocation','users.nom as nom','users.prenom as prenom','locations.dateL as dateL','locations.dateR as dateR','cars.prix as prix' )->whereIn('locations.idLocation', $ids)->orderBy('idLocation')->get();
+        ->select('locations.id','users.nom as nom','users.prenom as prenom','images.chemin as image','locations.dateL as dateL','locations.dateR as dateR','cars.prix as prix' )->whereIn('locations.idLocation', $ids)->orderBy('idLocation')->get();
     }
     public function loca()
     {
        
     return  DB::table('locations')
         ->join('cars', 'locations.car_id', '=', 'cars.id')
-        ->select('locations.idLocation','cars.prix as prix' )->get();
+        ->select('locations.id','cars.prix as prix' )->get();
+    }
+   
+    public function lid($i) {
+        return DB::table('locations')
+        ->join('users', 'users.id', '=', 'locations.user_id')
+            ->join('cars', 'cars.id', '=', 'locations.car_id')
+            ->join('marques', 'cars.marque_id', '=', 'marques.id')
+            ->join('images', 'cars.image_id', '=', 'images.id')
+            ->join('typemoteurs', 'cars.typemoteur_id', '=', 'typemoteurs.id')
+            ->join('typevoitures', 'cars.typevoiture_id', '=', 'typevoitures.id')
+          
+            ->where('locations.user_id', '=', $i)
+            ->select('images.chemin as zineb','marques.libelle as marque', 'typevoitures.libelle as typevoiture', 'typemoteurs.libelle as typemoteur', 'locations.dateL', 'locations.dateR')
+            ->get();
+    }
+
+    public function cp($i, $ii) {
+        return DB::table('locations')
+        ->join('cars', 'cars.id', '=', 'locations.car_id')
+        ->join('marques', 'cars.marque_id', '=', 'marques.id')
+        ->join('typemoteurs', 'cars.typemoteur_id', '=', 'typemoteurs.id')
+        ->join('typevoitures', 'cars.typevoiture_id', '=', 'typevoitures.id')
+            
+            ->where('locations.id', '=', $i)
+            ->where('cars.id', '=', $ii)
+            ->select(DB::raw('(DATEDIFF(locations.dateR, locations.dateL) + 1) * cars.Prix AS prix_location') ,'locations.id','cars.id','typevoitures.libelle as typevoiture' , 'typemoteurs.libelle as typemoteur' , 'marques.libelle as marque')
+            ->get();
+    }
+    public function idloc($i,$ii,$n,$nn ) {
+        return DB::table('locations')
+            
+            ->where('user_id', '=', $i)
+            ->where('car_id', '=', $ii)
+            ->where('dateL', '=', $n)
+            ->where('dateR', '=', $nn)
+            ->select('id')
+            ->get();
     }
     public function store(Request $request)
     {
         $request->validate([
-            'idLocation'=>'required',
-            'user_id	 '=>'required',
-            'car_id	 '=>'required',
-            'dateL '=>'required',
-            '	dateR '=>'required',
-        ]) ;
+            'user_id'=>'required',
+            'car_id'=>'required',
+            'dateL'=>'required',
+            'dateR'=>'required',
+        ]);
        Location::create($request->post());
         return response()->json([
             'message'=>'location  creer'
         ]);
     }
 
- 
-    public function show(Location $location)
-    {
-        return response()->json([
-            'location'=>$location
-        ]);
-       
+    public function calcul($id, $id2) {
+        return DB::table('locations')
+        ->join('cars','locations.car_id','=','cars.id')
+        ->where('locations.id', '=', $id)
+        ->where('locations.car_id', '=', $id2)
+        ->select(DB::raw('(DATEDIFF(locations.dateR, locations.dateL) + 1) * cars.Prix AS total') )
+        ->get();
+            
+    }
+    public function show(){
+        
+        return  DB::table('locations')
+        ->join('cars', 'locations.car_id', '=', 'cars.id')->join('images', 'cars.image_id', '=', 'images.id')
+        ->join('users', 'locations.user_id', '=', 'users.id')
+        
+        ->select('locations.id','users.nom as nom','users.prenom as prenom','images.chemin as image','locations.dateL as dateL','locations.dateR as dateR','cars.prix as prix' )->where('id','=',$id)->get();
     }
 
   
@@ -73,7 +119,7 @@ class LocationController extends Controller
     }
     public function search ($id)
 {
-return Location::where('idLocation', 'like','%' .$id. '%')->get();
+return Location::where('id', 'like','%' .$id. '%')->get();
 
 }
  public   function  UserId ($id)
@@ -82,4 +128,10 @@ return Location::where('idLocation', 'like','%' .$id. '%')->get();
         return    $luid;
     
 }
+public function lc($id)
+    {
+        return  DB::table('locations')
+        ->join('cars', 'locations.car_id', '=', 'cars.id')
+        ->select('locations.car_id')->where('locations.id','=',$id)->get();
+    }
 }
