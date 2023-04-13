ae6a9b033ac729f164caa897ecc3cdbb365dc12a
DevTCode <assimichraq@gmail.com>
2023-04-13 03:45:37 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,6 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
   
-  
-    
-    
    
     public function index()
     {
@@ -22,27 +19,46 @@ class UserController extends Controller
     {
         
     }
-
-    
-    public function update(Request $request, User $User)
+    public function update(Request $request, $id)
     {
         $request->validate([
-           
-            'tel'=>'required',
-          
-            'email'=>'required',
-           
-
+            'nom' => 'required',
+            'prenom' => 'required',
+            'CNE' => 'required',
+            'tel' => 'required',
+            'numPermis' => 'required',
+            'email' => 'required',
         ]);
-        $User->fill($request->post())->update();
-        return response()->json([
-            'message'=>'User updated successfuly'
-        ]);
+    
+        // Utilisez find() pour obtenir le modèle existant ou null si non trouvé
+        $user = User::find($id);
+    
+        if ($user) {
+            // Mettez à jour les attributs du modèle avec les données du formulaire
+            $user->nom = $request->input('nom');
+            $user->prenom = $request->input('prenom');
+            $user->CNE = $request->input('CNE');
+            $user->tel = $request->input('tel');
+            $user->numPermis = $request->input('numPermis');
+            $user->email = $request->input('email');
+            $user->save(); // Sauvegardez les modifications dans la base de données
+    
+            return response()->json([
+                'message' => 'Utilisateur mis à jour avec succès',
+                 
+            ]);
+        } else {
+            // Gérez le cas où le modèle n'est pas trouvé
+            return response()->json([
+                'message' => 'Utilisateur non trouvé'
+            ], 404);
+        }
     }
-    public function show(){
+    public function show($id){
         
         return  DB::table('users')
-        ->select('tel','email')->get();
+        ->where('id','=',$id)
+        ->select('nom','prenom','CNE','tel','numPermis','email')->get();
     }
      
       
