3bed524b80b4b07a04846c9cfcdda18a128230f0
DevTCode <assimichraq@gmail.com>
2023-04-13 03:47:47 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\typevoitureController;
 use App\Http\Controllers\API\AdminController;
 use App\Http\Controllers\API\ClientController;
 use App\Http\Controllers\LocationController;
+use App\Http\Controllers\factureController;
 
 
 /*
@@ -45,13 +46,26 @@ Route::resource('marques',marqueController::class);
 Route::resource('typemoteur',typemoteurController::class);
 Route::resource('typevoiture',typevoitureController::class);
 Route::resource('clients',UserController::class);
+Route::get('factures',[factureController::class,'index']);
 Route::resource('locations',locationController::class);
+//Route::get('sh/{id}',[locationController::class],'show');
+//Route::resource('factures',factureController::class);
 //Route::resource('ima',imageController::class);
-
+Route::get('lc/{id}',[locationController::class],'lc');
+Route::get('lm',[ marqueController::class,'lm']);
+Route::get('ltv',[ typevoitureController::class,'index2']);
+Route::get('ltm',[ typemoteurController::class,'index4']);
+Route::get('all/{name}/{name2}/{name3}',[  CarController::class,'searchbyall']);
+Route::get('mtv/{name}/{name2} ',[  CarController::class,'searchbymtv']);
+ Route::get('mtm/{name}/{name2} ',[  CarController::class,'searchbymtm']);
+ Route::get('tvtm/{name}/{name2} ',[  CarController::class,'searchbytvtm']);
 Route::post('upload', [imageController::class,'store']);
+Route::put('edituser/{id}',[userController::class,'update']);
 Route::get('allPic', [imageController::class,'show']);
 Route::get('loc', [locationController::class,'loc']);
-Route::get('loca', [locationController::class,'loca']);
+Route::get('calcul/{id}/{id2}',[locationController::class,'calcul']);
+Route::get('lc/{id}',[locationController::class,'lc']);
+Route::get('loca/{id}', [factureController::class,'loca']);
 Route::get('tv', [typevoitureController::class,'tv']);
 Route::get('allIm', [imageController::class,'sh']);
 Route::get('allIs', [imageController::class,'sho']);
@@ -71,14 +85,18 @@ Route::get('/type_voiture', [typevoitureController::class, 'getTypevoiture']);
 Route::get('/mar/{id}', [marqueController::class, 'getMarqueLibelle']);
 Route::get('/tms/{id}', [typemoteurController::class, 'getTmLibelle']);
 Route::get('/tvs/{id}', [typevoitureController::class, 'getTvLibelle']);
+Route::get('al',[ CarController::class,'searchbyal']);
 Route::get('all',[ CarController::class,'searchbyall2']);
 Route::get('allCars',[ CarController::class,'allCars']);
 Route::get('c',[ CarController::class,'c']);
+Route::post('res',[ factureController::class,'store']);
 
- Route::post('/register',[ClientController::class,'register']);
- Route::post('/login',[ClientController::class,'login']);
- Route::post('/logout',[ClientController::class,'logout']);
- Route::post('/reg',[AdminController::class,'register']);
+Route::post('login',[ClientController::class,'login']);
+Route::get('logout',[ClientController::class,'logout'])->middleware('auth:sanctum');
+Route::get('/usid', [ClientController::class, 'id'])->middleware('auth:sanctum');
+Route::post('/reg',[AdminController::class,'register']);
+Route::post('register',[ClientController::class,'register']);
+Route::get('ic/{id}',[ClientController::class,'ic']);
  Route::post('/log',[AdminController::class,'login']);
  Route::post('/lgout',[AdminController::class,'logout'])->middleware('auth:sanctum');
  Route::get('/api/options', [CarController::class,'cr']);
@@ -87,15 +105,25 @@ Route::get('c',[ CarController::class,'c']);
 
  Route::get('stv/{name}',[ typevoitureController::class,'searchbytv']);
  Route::get('stm/{name}',[ typemoteurController::class,'searchbytm']);
- 
- 
+ Route::post('z2',[ locationController::class,'store']);
 
+Route::get('lid/{i}',[ locationController::class,'lid']);
+Route::put('di/{id}',[CarController::class,'updateCarDispo']);
+Route::get('prix/{i}/{ii}',[ locationController::class,'cp']);
+Route::get('idloc/{i}/{ii}/{n}/{nn}',[ locationController::class,'idloc']);
  
-
+Route::get('show/{id}',[UserController::class,'show']);
+Route::put('edituser/{id}',[UserController::class,'update']);
+ 
+Route::get('f/{i}',[ factureController::class,'index4']);
  Route::get('z',[ CarController::class,'dispo']);
-Route::get('z2/{id}',[ LocationController::class,'UserId']);
+
 Route::get('{name}',[ CarController::class,'searchbymarque']);
-Route::get('zz/{name}',[ CarController::class,'searchbytypemoteur']);
-Route::get('z/{name}',[ CarController::class,'searchbytypevoiture']);
-Route::get('lm',[ marquesController::class,'index2']);
+ Route::get('zz/{name}',[ CarController::class,'searchbytypemoteur']);
+ Route::get('z/{name}',[ CarController::class,'searchbytypevoiture']);
+
+//Route::get('all',[ CarController::class,'searchbyall2']);
+
+
+
 
